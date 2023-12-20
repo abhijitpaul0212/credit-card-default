9359020a168596827a2b6a332bbaa6c409c97bc8
Abhijit Paul <abhijitpaul0212@gmail.com>
2023-12-21 00:29:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ data.drop(['DEFAULT_PAYMENT', "_id", "ID"], axis=1, inplace=True)
 
 # Design Streamlit Page
 st.write("""
-# Credit Card Defaults Prediction
-This app predicts the **Credit Card Defaults**!
+# Credit Card Default Detection
+This app predicts the **Credit Card Payment Defaults**!
 """)
 st.write('---')
 
